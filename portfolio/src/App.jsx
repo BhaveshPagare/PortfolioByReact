f974845cae3f98fa125c5b98fe4ec05dff08c0bd
bhavesh <149988236+BhaveshPagare@users.noreply.github.com>
2024-03-16 05:31:54 +0000

    directory done........

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Contact from './components/Contact'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar></Navbar>
+      {/* <About></About> 
+      <Contact></Contact> */}
+
+    </>
+  )
+}
+
+export default App
